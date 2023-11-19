bccffb7c1e4519f767283c416c1920ce10452f16
julianatalita <jts4@cin.ufpe.br>
2023-11-18 22:14:34 -0300

    Final Project!

--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -20,7 +23,7 @@ def home(request):
         else:
             messages.error(request, "Usuário e senha são obrigatórios.")
     
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
@@ -29,4 +32,68 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Você foi cadastrado! Bem-vindo!")
+
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.error(request, "Você precisa entrar primeiro para visualizar essa página.")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Cliente apagado com sucesso!")
+        return redirect('home')
+    else:
+        messages.error(request, "Você precisa entrar primeiro para excluir um cliente.")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Seu cliente foi adicionado.")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.error(request, "Você precisa entrar primeiro para adicionar um cliente.")
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cliente atualizado com sucesso!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.error(request, "Você precisa entrar primeiro para atualizar clientes.")
+        return redirect('home')
+
+
